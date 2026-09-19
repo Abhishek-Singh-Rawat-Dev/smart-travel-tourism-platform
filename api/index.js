3cc008bf2bda7e37005aa4dbda19c852ad16b72d
Abhishek Singh Rawat <scientist1420@gmail.com>
2026-09-19 13:13:09 +0530

    feat: add api/index.js URL normalizer and vercel rewrite configuration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-// Vercel Serverless Function Handler
 const app = require('../backend/server');
 
-module.exports = app;
+module.exports = (req, res) => {
+    // Normalise request URL so Express routes mounted at /api match seamlessly
+    if (!req.url.startsWith('/api')) {
+        req.url = '/api' + req.url;
+    }
+    return app(req, res);
+};
