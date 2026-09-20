d50a067bdb4591a7f33ce3b886c19e22a8c05a82
Abhishek Singh Rawat <scientist1420@gmail.com>
2026-09-20 21:54:45 +0530

    fix: normalize Vercel serverless req.url to prepend /api if stripped by rewrite

--- api/index.js
+++ api/index.js
@@ -5,5 +5,8 @@ mongoose.set('autoIndex', false);
 const app = require('../backend/server');
 
 module.exports = (req, res) => {
+    if (req.url && !req.url.startsWith('/api')) {
+        req.url = '/api' + (req.url.startsWith('/') ? '' : '/') + req.url;
+    }
     return app(req, res);
 };
