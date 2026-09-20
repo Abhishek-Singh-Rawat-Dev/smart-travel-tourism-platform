f4ed7abb14010271d60dc8219a691b7a1f15feca
Abhishek Singh Rawat <scientist1420@gmail.com>
2026-09-20 22:21:49 +0530

    fix: use routes in vercel.json to preserve request URL for express server

--- api/index.js
+++ api/index.js
@@ -4,16 +4,6 @@ mongoose.set('autoIndex', false);
 
 const app = require('../backend/server');
 
-const url = require('url');
-
 module.exports = (req, res) => {
-    try {
-        const parsed = url.parse(req.url || '', true);
-        if (parsed.query && parsed.query.path) {
-            req.url = '/api/' + parsed.query.path;
-        } else if (req.headers['x-matched-path']) {
-            req.url = req.headers['x-matched-path'];
-        }
-    } catch (e) {}
     return app(req, res);
 };
